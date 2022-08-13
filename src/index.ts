5ab4225a3e320a263a2b6987bcec3c0534b81f6f
Vedant Bothikar <vbothikar2001@gmail.com>
2022-08-13 23:30:25 +0530

    classes done

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 let i: number = 5;
 
-console.log(i);
+// console.log(i);
 
 // basic types
 let j: string = "hello";
@@ -27,7 +27,7 @@ mytuparr = [
   [3, true],
 ];
 
-console.log(mytuparr);
+// console.log(mytuparr);
 
 // UNION
 let n: string | number;
@@ -55,3 +55,49 @@ function myfunc(x: number, y: number): number {
 function myfunc2(x: string | number): void {
   console.log(x);
 }
+
+// Interfaces
+
+// Question mark means the property is optional
+interface UserInterface {
+  id: number;
+  name: string;
+  age?: number;
+}
+
+const user2: UserInterface = {
+  id: 1,
+  name: "vedant",
+};
+
+// using interfaces with functions
+
+// here we are setting the parameter and return type format
+interface mathfunc {
+  (x: number, y: number): number;
+}
+
+// mathfunc will check if the myadd function has same parameter and return type format
+const myadd: mathfunc = (x: number, y: number): number => {
+  return x + y;
+};
+
+const mysub: mathfunc = (x: number, y: number): number => {
+  return x - y;
+};
+
+// Classes
+
+class Person {
+  id: number;
+  name: string;
+
+  constructor(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+}
+
+const vedant = new Person(1, "vedant");
+
+// console.log(vedant);
